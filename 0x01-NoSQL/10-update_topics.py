beb85338bb6c5712fe1fd904a5d3fdf7a26febd2
opxzy22 <aopeyem84@gmail.com>
2024-01-26 02:33:05 -0800

    update

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+a function that changes all topic of school
+document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    mongo collection:
+                    the pymongo collection object
+    name:
+        (string) the name of school to update
+    topics:
+        (list of strings) the list of topics to be updated
+    """
+    mongo_collection.update_many({"name": name}, {$set: {"topic": topic}})
